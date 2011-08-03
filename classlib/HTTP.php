54989ed089e34f36df48bde0ef03c061309d4fd5
Halfdeck@HALFDECK-PC <Halfdeck@HALFDECK-PC>
2011-08-02 20:19:02 -0700

    merging with job board changes.

--- classlib/HTTP.php
+++ classlib/HTTP.php
@@ -5,28 +5,43 @@
  * ROCKETS_URL handles URL parsing, not HTTP header checks</p>
  * 
  */
-class ROCKETS_HTTP {
+class ROCKETS_HTTP
+{
     const HTTP_HEADER_200 = "HTTP/1.1 200 OK";
+
+    /**
+     * Simple redirect
+     * @param type $path 
+     */
+    static public function redirect($path)
+    {
+        header("Location:" . $path);
+        exit(); // without exit() redirect may die when sessions are active.
+    }
 
     /**
      * <p>Generic function that checks for broken links.</p>
      * @param string $url - e.g. http://www.cnn.com/
      * @return boolean true if URL is valid, false otherwise.
      */
-    public static function http_isValidURL($url) {
-	if(!ROCKETS_URL::isWellFormedURL($url)) {
-	    if(BOOL_DEBUG) {
-		echo "URL is NOT WELL FORMED!" .PHP_EOL;
-	    }
-	    return false;
-	}
-	if(BOOL_DEBUG) {
-	    echo "URL is well formed!" .PHP_EOL;
-	}
-	$headers = get_headers($url);
-	if ($headers[0] != self::HTTP_HEADER_200)
-	    return false;
-	return true;
+    public static function http_isValidURL($url)
+    {
+        if (!ROCKETS_URL::isWellFormedURL($url))
+        {
+            if (BOOL_DEBUG)
+            {
+                echo "URL is NOT WELL FORMED!" . PHP_EOL;
+            }
+            return false;
+        }
+        if (BOOL_DEBUG)
+        {
+            echo "URL is well formed!" . PHP_EOL;
+        }
+        $headers = get_headers($url);
+        if ($headers[0] != self::HTTP_HEADER_200)
+            return false;
+        return true;
     }
 
 }
