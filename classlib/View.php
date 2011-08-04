5dafdfd318fac3340e60fe5c2c6b0aa1f1523556
Halfdeck@HALFDECK-PC <Halfdeck@HALFDECK-PC>
2011-08-04 00:33:15 -0700

    testing phpdocumentor

--- classlib/View.php
+++ classlib/View.php
@@ -13,9 +13,19 @@ class ROCKETS_View
 {
 
     /**
-     * Default __get
+     * <p>use __get() to retrieve property data
+     * 
+     * example: <code>echo $job->date_due;</code>
+     * 
+     * <b>Usage:</b> To trigger this method, you must protect the property in class header.
+     * The protection forces __get to be triggered.
+     * 
+     * <code>protected $date_due, first_name;</code>
+     * 
+     * </p>
+     * 
      * @param type $name
-     * @return type
+     * @return type 
      */
     public function __get($name)
     {
@@ -56,15 +66,18 @@ class ROCKETS_View
     }
 
     /**
-     * Load record and populate properties.
+     * <p>Load record and populate properties.
      * For example, given a JOB_HTML_Letter class, return an JOB_HTML_Letter
-     * object with pre-filled properties based on $id
+     * object with pre-filled properties based on $id and data retrieved via 
+     * JOB_MODEL_Letter. The actual handling of properties are in __get(),
+     * so the template page isn't littered with extraneous code.
      * 
-     * <h3>Usage</h3>
-     * <pre>
-     * $user = JOB_HTML_User->load_object($id);
+     * <b>Usage</b>
+     * <code>
+     * $user = JOB_HTML_User::load_object($id);
      * echo $user->full_name;
-     * </pre>
+     * </code>
+     * </p>
      * 
      * @param <type> $id
      * @return object
