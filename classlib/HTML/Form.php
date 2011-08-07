72d7ffab28891ff405e6757b4db4bf52566ee1ca
Halfdeck@HALFDECK-PC <Halfdeck@HALFDECK-PC>
2011-08-07 06:59:42 -0700

    changed draw_select to static so I don't have to instantiate an object

--- classlib/HTML/Form.php
+++ classlib/HTML/Form.php
@@ -3,54 +3,59 @@
 /**
  * HTML ROCKETS_HTML_Form object
  */
-class ROCKETS_HTML_Form {
+class ROCKETS_HTML_Form
+{
+    /**
+     * input types
+     */
+    const IT_CHECKBOX = 0;
+    const IT_TEXT = 1;
 
-	/**
-	 * input types
-	 */
-	const IT_CHECKBOX = 0;
-	const IT_TEXT = 1;
+    /**
+     * Draw a listbox given an array of values.
+     *
+     * (array(
+     * 	"name" => "",
+     *  "options" => "",
+     *  "checked" => "",
+     *  "class" => ""
+     * ));
+     * 
+     * @param <type> $ar
+     */
+    static public function draw_select($ar = array(null))
+    {
+        /**
+         * Optional class string - for custom styling
+         */
+        if (isset($ar['class']))
+            $classStr = "class='{$ar['class']}'";
+        else
+            $classStr = "";
 
-	/**
-	 * Draw a listbox given an array of values.
-	 *
-	 * (array(
-     *	"name" => "",
-	 *  "options" => "",
-	 *  "checked" => "",
-	 *  "class" => ""
-	 * ));
-	 * 
-	 * @param <type> $ar
-	 */
-    public function draw_select($ar = array(null)) {
-		/**
-		 * Optional class string - for custom styling
-		 */
-	if(isset($ar['class'])) $classStr = "class='{$ar['class']}'";
-	else $classStr = "";
+        echo "<select name='{$ar['name']}' {$classStr}>";
+        foreach ($ar["options"] as $key => $val)
+        {
+            $selected = "";
+            if ($key == $ar["checked"])
+                $selected = " selected=\"selected\"";
+            echo "		<option value='" . $key . "' {$selected}>" . $val . "</option>\n";
+        }
+        echo "</select>";
+    }
 
-		echo "<select name='{$ar['name']}' {$classStr}>";
-	foreach ($ar["options"] as $key => $val) {
-			$selected = "";
-			if ($key == $ar["checked"])
-				$selected = " selected=\"selected\"";
-			echo "		<option value='" . $key . "' {$selected}>" . $val . "</option>\n";
-		}
-		echo "</select>";
-	}
+    /** Draw search textbox
 
-	/** Draw search textbox
+      params:
 
-	  params:
-
-	  size
-	  name
-	  value
-	 * */
-    public function draw_searchbox($ar = array(null)) {
-		echo "<input type='text' name='{$ar['name']}' size='{$ar['size']}' value='{$ar['value']}'>";
-	}
+      size
+      name
+      value
+     * */
+    public function draw_searchbox($ar = array(null))
+    {
+        echo "<input type='text' name='{$ar['name']}' size='{$ar['size']}' value='{$ar['value']}'>";
+    }
 
 }
 
