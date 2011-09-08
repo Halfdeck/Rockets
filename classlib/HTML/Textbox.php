a6f65269587aab94cd9afdae917d4b877db60c64
Tetsuto <tetsuto@infotrackit.com>
2011-09-07 17:14:59 -0700

    - added an optional parameter that controls read only setting

--- classlib/HTML/Textbox.php
+++ classlib/HTML/Textbox.php
@@ -8,15 +8,20 @@
 class ROCKETS_HTML_Textbox extends ROCKETS_HTML_Form 
 {
 	static $input_type = self::TYPE_INPUT_TEXT;
+	const STR_READ_ONLY = "readonly='readonly'";
+	
     /**
      * Auto-draws a textbox INPUT, using a class object
      * 
      * @param type $name name of the INPUT
      * @param type $obj object used to retrieve value
+	 * @param boolean $options['read only'] activate read only 
      */
-    static public function draw_obj($name, $obj)
+    static public function draw_obj($name, $obj, $options = array(null))
     {
-        echo "<input type='" .self::$input_type ."' name='{$name}' value='{$obj->$name}'/>";
+		$readonly = (isset($options['read only']) && $options['read only'] == TRUE) ? self::STR_READ_ONLY : "";
+		
+        echo "<input type='" .self::$input_type ."' name='{$name}' value='{$obj->$name}' {$readonly}/>";
     }
 	
 	/**
