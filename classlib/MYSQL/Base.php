94dfbe40098c4640454107ce8934335e1b702434
Tetsuto <tetsuto@infotrackit.com>
2011-09-14 07:22:29 -0700

    - pulled down MVC-related classes into rockets.

---
+++ classlib/MYSQL/Base.php
@@ -0,0 +1,334 @@
+<?php
+
+/**
+ * Fundamental MYSQL methods - A portion of ROCKETS_MYSQLTable
+ *
+ * @author Halfdeck
+ */
+class ROCKETS_MYSQL_Base {
+	/**
+	 * @DUPLICATE Duplicates ROCKETS_MYSQLTable
+	 * 
+	 * Fieldnames are all duplicates
+	 * 
+	 */
+
+	/**
+	 * @MOD 
+	 * Referenced by View 
+	 * Used to auto-generate mutli-table queries
+	 * 
+	 * @var type 
+	 */
+	public $alias;
+
+	/** name of this table */
+	protected $tbl;
+	public $foreign_keys;
+	public $parent;
+
+	/**
+	 * @MOD set to private to force lazy-loading during insert/delete, etc
+	 *  
+	 * name of primary key field */
+	private $primary_key_fieldname;
+
+	/**
+	 * table META data (fieldnames)
+	 * @MOD force lazy load
+	 */
+	private $fieldnames = array();
+
+	/**
+	 * Used to maintain primary key(s) in cases where more than one key is used
+	 * @var Array
+	 */
+	private $primary_keys = array();
+
+	/**
+	 * An array of foreign keys.
+	 * Structure
+	 * [foreign_key] => array(
+	 * 	 'table'=> [tablename],
+	 *   'key' => [foreign_key]
+	 *
+	 * These keys are static so we can instantly access them without instantiating an object. Since keys of multiple tables
+	 * must be looked up in a single query, creating objects could be very slow.
+	 *
+	 * @var <type>
+	 */
+	static $keys_foreign = array();
+
+	/**
+	 * @MODIFIED 
+	 * prevented auto-loading
+	 */
+	public function __construct($ar = array(null))
+	{
+		
+	}
+
+	/**
+	 * @DUPLICATE Duplicates ROCKETS_MYSQLTable
+	 * @param type $ar 
+	 */
+	protected function loadQueryValues($ar = array(null))
+	{
+		foreach ($ar as $key => $value)
+		{
+			if (is_array($value))
+			{
+				$this->$key = $value;
+			}
+			else
+			{
+				$this->$key = strtolower($value); // URL parameters are all lower case, so form values need to be set to lower case also
+			}
+		}
+	}
+
+	/**
+	 * @DUPLICATE Duplicates ROCKETS_MYSQLTable
+	 * 
+	 * A non-static version of retrieveTableFields() - for accessing primary key
+	 * @param <type> $tableName
+	 * @return <type>
+	 */
+	protected function getTableFields($tableName)
+	{
+
+		$tableFields = self::read("SHOW COLUMNS FROM {$tableName}");
+
+		while ($field = mysql_fetch_assoc($tableFields))
+		{
+
+			$field_array[] = $field['Field'];
+			if ($field['Key'] == 'PRI')
+			{
+				$this->primary_key_fieldname = $field['Field']; // set primary key
+				$this->primary_keys[] = $field['Field'];
+			}
+		}
+		return $field_array;
+	}
+
+	/**
+	 * @DUPLICATE Duplicates ROCKETS_MYSQLTable
+	 * 
+	 * Select statement wrapper - used to generate feedback AND possibly sanitize MYSQL queries for security
+	 * @param <type> $query
+	 * @return <type>
+	 */
+	static public function read($query)
+	{
+		if (BOOL_DEBUG)
+		{
+			echo ROCKETS_String::mysql_prettify($query);
+		}
+		$result = mysql_query($query);
+		self::issueError(array("continue" => true, "query" => $query));
+
+		return $result;
+	}
+
+	/**
+	 * @NEW
+	 * 
+	 * get method, used for lazy loading properties, like table METAs.
+	 * Do not use this to fish for table data
+	 * 
+	 * @param type $name 
+	 */
+	public function __get($name)
+	{
+
+		switch ($name) {
+			case "primary_keys":
+			case "primary_key_fieldname":
+				if (empty($this->fieldnames) && $this->$name == null)
+				{
+					$this->getTableFields($this->tbl);
+					return $this->$name;
+				}
+				else
+					return $this->$name;
+				break;
+			case "tbl":
+				if ($this->tbl == null)
+				{
+					trigger_error('set MYSQL table name in $tbl', E_USER_ERROR);
+				}
+				else
+				{
+					return $this->tbl;
+				}
+				break;
+			case "fieldnames":
+				if (empty($this->fieldnames))
+				{
+					$this->fieldnames = $this->getTableFields($this->tbl);
+					return $this->fieldnames;
+				}
+				else
+				{
+					return $this->fieldnames;
+				}
+				break;
+			case "foreign_keys":
+				return $this->$name;
+				break;
+			default:
+				if (isset($this->$name))
+					return $this->name;
+				else
+					return null;
+				break;
+		}
+	}
+
+	/**
+	 * This allows child classes to set private member values
+	 * 
+	 * @param type $name
+	 * @param type $value 
+	 */
+	public function __set($name, $value)
+	{
+		$this->$name = $value;
+	}
+
+	/**
+	 * @DUPLICATE Duplicates ROCKETS_MYSQLTable
+	 * 
+	 * MYSQL write operation wrapper function, used to handle debugging. Write operations include
+	 * INSERT, UPDATE, CREATE TABLE, etc.
+	 *
+	 * @param string $query - query string
+	 */
+	public static function exec($query)
+	{
+		if (BOOL_DEBUG)
+			echo ROCKETS_String::mysql_prettify($query);
+		if (BOOL_EXECUTE)
+		{
+			mysql_query($query);
+			self::issueError(array("continue" => false, "query" => $query));
+		}
+	}
+
+	/**
+	 * Default get_array() class
+	 * Return an array containing a list of items
+	 * This is used primarily for building a listbox
+	 *
+	 * Usage: The field names should be "name" and "id" - otherwise this won't work.
+	 *
+	 * @return array associative array, "user_id"=>"full_name"
+	 */
+	public static function get_array()
+	{
+		$result = self::read("SELECT DISTINCT name, id  FROM " . self::constructTableNameByClassName() . " WHERE 1 ORDER BY name ASC");
+
+		if (!$result || mysql_num_rows($result) == 0)
+			return null;
+
+		while ($row = mysql_fetch_assoc($result))
+		{
+			$ar[$row['id']] = $row['name'];
+		}
+		return $ar;
+	}
+
+	/**
+	 * Creates a MYSQL table name, given a class name.
+	 * This is used for classes calling static methods
+	 *
+	 * Usage: 
+	 * 
+	 * This method is called internally - it will NOT work if its moved to a String class, for example.
+	 * 
+	 * cases:
+	 *
+	 * MODEL_Job -> jobs
+	 * MODEL_Company -> companies
+	 * MODEL_PostageAccount -> postage_accounts
+	 *
+	 * @param $class_name (optional) class name
+	 * @return type
+	 */
+	static protected function constructTableNameByClassName($class_name = null)
+	{
+		if (empty($class_name))
+			$class_name = get_called_class(); // get class name "JOB_MODEL_PostageAccount"
+
+		$parts = explode("_", $class_name);
+		$table_name = $parts[count($parts) - 1]; // get the table name singular, like "PostageAccount"
+		$table_name = ROCKETS_String::unCamelCase($table_name); // uncamel case it -> "Postage Account"
+		$table_name = trim($table_name); // " Postage Account" -> "Postage Account";
+		$table_name = str_replace(" ", "_", $table_name); // put separator -> "Postage_Account"
+		$table_name = ROCKETS_String::makePlural($table_name); // make it plural -> "Postage_Accounts"
+		$table_name = strtolower($table_name); // "postage_accounts"
+
+		return $table_name;
+	}
+
+	/**
+	 * 
+	 * @DUPLICATE Duplicates ROCKETS_MYSQLTable
+	 * Issue an error and exit
+	 *
+	 * @param string $ar['query'] REQUIRED - MYSQL query string
+	 * @param boolean $ar['continue'] OPTIONAL - if false, die on error.
+	 *
+	 * $continue - if false, die.
+	 */
+	static protected function issueError($ar = null)
+	{
+		if (mysql_errno())
+		{
+			if (isset($ar['query']))
+				echo "<p><strong>Error in query:</strong>{$ar['query']}</p>";
+			echo mysql_error() . "<br>";
+			if (array_key_exists("continue", $ar) && $ar['continue'] == false)
+				die();
+		}
+	}
+
+	/**
+	 * @DUPLICATE ROCKETS_MYSQLTable
+	 * @MODIFIED (simplified code)
+	 * 
+	 * count total number of rows returned by a MYSQL query
+	 *
+	 * @param
+	 *
+	 * $verbose - set to true to see number of rows returned by a select statement
+	 */
+	protected function countRows($verbose = false)
+	{
+		$result = mysql_fetch_assoc(self::read("SELECT FOUND_ROWS()"));
+		$this->rowCount = $result['FOUND_ROWS()'];
+	}
+
+	/**
+	 * Return number of returned rows
+	 * 
+	 * @return int number of rows in MYSQL result
+	 */
+	public function get_rowCount()
+	{
+		return $this->rowCount;
+	}
+
+	/**
+	 * Alias for mysql_insert_id();
+	 * @return type
+	 */
+	static public function getLastInsertedID()
+	{
+		return mysql_insert_id();
+	}
+
+}
+
+?>
---
+++ classlib/MYSQL/TwoPass.php
@@ -0,0 +1,458 @@
+<?php
+
+/**
+ * Handles relational MYSQL using a Two-Pass method
+ *
+ * @author Aaron
+ */
+class ROCKETS_MYSQL_TwoPass extends ROCKETS_MYSQL_Query {
+	/**
+	 * Do a regular count(*) - used when making a new JOIN using primary key
+	 */
+	const MODE_COUNT = 1;
+
+	/**
+	 * Do a SUM(COUNT(*) - used when making a JOIN not using a primary key
+	 */
+	const MODE_SUMCOUNT = 2;
+
+	/**
+	 * Group concat
+	 */
+	const MODE_GROUPCONCAT = 3;
+
+	/**
+	 * $GLOBALS array key used to maintain FROM clauses
+	 */
+	const GLOBAL_KEY_FROM_CLAUSES = 'from_clauses';
+
+	/**
+	 * $GLOBALS key used to maintain SELECT clauses
+	 */
+	const GLOBAL_KEY_SELECT_CLAUSES = 'select_clauses';
+
+	public $foreign_keys = array();
+	public $parent = "";
+
+	/**
+	 * Fields meta array used to map result fields when we use multiple tables
+	 * and field name collisions are possible
+	 * 
+	 * @var Array
+	 */
+	static public $fieldsMeta = array();
+
+	/**
+	 * This construct blocks normal construct operation of parent classes
+	 */
+	public function __construct($ar = array(null))
+	{
+		if (BOOL_DEBUG)
+			echo "Model born!" . get_class($this) . "<bR>";
+		parent::__construct($ar);
+	}
+
+	public function set_parent(ROCKETS_Model $parent = null)
+	{
+		$this->parent = $parent;
+	}
+
+	/**
+	 * Auto-create Data Element filename used for two-pass scanning
+	 */
+	static private function get_eval_filename()
+	{
+		$return_str = self::constructTableNameByClassName() . ".php"; // 'RoleTask' => 'role_tasks.php'
+		$return_str = str_replace('_', '-', $return_str); // 'role_tasks.php' => 'role-tasks.php'
+		return $return_str;
+	}
+
+	/**
+	 * First pass.. scan the Element Data file and load data into $GLOBALS
+	 * 
+	 * @param string $filename 
+	 */
+	static public function load_global($filename = null)
+	{
+		if (empty($filename))
+		{
+			$filename = self::get_eval_filename();
+		}
+		if (BOOL_DEBUG)
+			echo "GOT FILENAME: {$filename}<bR>";
+		/**
+		 * Supress output here .. just get $GLOBALS['tables'], an array of JOIN clauses
+		 */
+		ob_start(); // eval needs to get captured in a string - regular $message = eval assignment won't work.
+		eval('?>' . file_get_contents(PATH_ELEMENTS_DATA . "/{$filename}") . '<?');
+		$s = ob_get_contents();
+		ob_end_clean();
+	}
+
+	/**
+	 * Create FROM clause using $GLOBALS
+	 * 
+	 * @return string 
+	 */
+	public function get_from_clause_two_pass()
+	{
+		$from_clause = "FROM {$this->tbl} as {$this->alias}";
+
+		foreach ($GLOBALS[self::GLOBAL_KEY_FROM_CLAUSES] as $clause)
+		{
+			$from_clause .= $clause;
+		}
+		return $from_clause;
+	}
+
+	public function get_select_clause_two_pass()
+	{
+		$select_clause = "*";
+		foreach ($GLOBALS[self::GLOBAL_KEY_SELECT_CLAUSES] as $clause)
+		{
+			$select_clause .= ",{$clause}";
+		}
+		return $select_clause;
+	}
+
+	public function get_group_by_clause_two_pass($select_clause)
+	{
+		/**
+		 * if select clause has a Count, you need to group by
+		 */
+		$group_by_clause = "";
+
+		if ($select_clause != "*")
+		{
+			$group_by_clause = "GROUP BY {$this->alias}.id";
+		}
+		return $group_by_clause;
+	}
+
+	/**
+	 * Generates a result set using a two-pass method.
+	 * 
+	 * @param type $ar
+	 * @return type 
+	 */
+	public function relational_two_pass($ar = array(null))
+	{
+		/**
+		 * GLOBALS['tables'] tracks an array of JOIN clauses
+		 * Select clauses are also partially auto-generated in cases where count()
+		 * need to be generated
+		 */
+		$GLOBALS[self::GLOBAL_KEY_FROM_CLAUSES] = array();
+		$GLOBALS[self::GLOBAL_KEY_SELECT_CLAUSES] = array();
+
+		if (isset($ar['filename']))
+			$filename = $ar['filename'];
+		else
+			$filename = null;
+
+		self::load_global($filename);
+
+		$select_clause = $this->get_select_clause_two_pass();
+		$from_clause = $this->get_from_clause_two_pass();
+		$where_clause = $this->get_where_clause($ar);
+		$group_by_clause = $this->get_group_by_clause_two_pass($select_clause);
+		$sort_order = $this->get_sort_clause($ar);
+
+		/**
+		 * Aliased tbl_users as u to do a nested select, to fish out company_name
+		 */
+		$result = self::read("SELECT SQL_CALC_FOUND_ROWS {$select_clause}
+			{$from_clause}
+			WHERE 1 {$where_clause}
+			{$group_by_clause}
+			{$sort_order}");
+
+		$this->countRows();
+		self::$fieldsMeta = self::getResultFields($result);
+
+		return $result;
+	}
+
+	/**
+	 * Auto-generate Count select subquery
+	 * USAGE: in an element/data/ file, evoke:
+	 * 
+	 * $x = new JOB_TEST_TestSQL;
+	 * $x->User->Invoice->count;
+	 * 
+	 * this will count the number of invoices associated with a user.
+	 * @param int $mode either self::MODE_COUNT or MODE_SUMCOUNT 
+	 */
+	public function create_select_count($mode = self::MODE_COUNT)
+	{
+		if (isset($this->parent->foreign_keys))
+		{
+			$parent_fkeys = $this->parent->foreign_keys;
+		}
+		else
+		{
+			return null;
+		}
+
+		if (isset($parent_fkeys[$this->alias]))
+		{
+			$parent_key = $parent_fkeys[$this->alias]['key'];
+			if (isset($this->parent->alias))
+				$parent_alias = $this->parent->alias;
+			else
+				$parent_alias = $this->parent->tbl;
+
+			$alias_clause = $this->construct_alias($this, self::MODE_COUNT, null);
+
+			$subquery = "(SELECT count(*) 
+				FROM {$this->alias} 
+				WHERE {$parent_alias}.{$parent_key} = {$this->alias}.{$parent_fkeys[$this->alias]['fkey']})";
+
+			switch ($mode) {
+				case self::MODE_COUNT:
+					$query = $subquery . " AS {$alias_clause}";
+					break;
+				case self::MODE_SUMCOUNT:
+					$query = "SUM({$subquery}) AS {$alias_clause}";
+					break;
+				default:
+					trigger_error("WRONG MODE - either MODE COUNT OR MODE SUMCOUNT", E_USER_ERROR);
+					break;
+			}
+
+			if (!in_array($query, $GLOBALS[self::GLOBAL_KEY_SELECT_CLAUSES]))
+			{
+				$GLOBALS[self::GLOBAL_KEY_SELECT_CLAUSES][] = $query;
+			}
+		}
+	}
+
+	public function create_query()
+	{
+		if (isset($this->parent->foreign_keys))
+		{
+			$parent_fkeys = $this->parent->foreign_keys;
+		}
+		else
+		{
+			/**
+			 * @todo the following IF clause never triggers - rewrite this to be relevant
+			 */
+			if (get_parent_class() == "JOB_TEST_TestSQL" && __CLASS__ != "JOB_TEST_TestSQL")
+			{
+				$GLOBALS[self::GLOBAL_KEY_FROM_CLAUSES][] = "FROM {$this->tbl}";
+			}
+			return null;
+		}
+
+		if (isset($parent_fkeys[$this->alias]))
+		{
+			$parent_key = $parent_fkeys[$this->alias]['key'];
+			if (isset($this->parent->alias))
+				$parent_alias = $this->parent->alias;
+			else
+				$parent_alias = $this->parent->tbl;
+
+			/**
+			 * Check for duplicate subqueries
+			 */
+			$subquery = " LEFT JOIN {$this->tbl} AS {$this->alias}";
+			$query = " LEFT JOIN {$this->tbl} AS {$this->alias} ON {$parent_alias}.{$parent_key} = {$this->alias}.{$parent_fkeys[$this->alias]['fkey']} ";
+
+			if (self::is_duplicate_query($subquery, $query) == FALSE)
+			{
+
+				$GLOBALS[self::GLOBAL_KEY_FROM_CLAUSES][] = $query;
+			}
+		}
+	}
+
+	/**
+	 * Check to see if the $query we constructed is new, or is already in the query array.
+	 * We check both subquery and $query. Subquery is "JOIN x as xx", Query is
+	 * the whole string "JOIN x as XX ON ....."
+	 * We check subquery because we can have multiple queries with identical subqueries
+	 * and those queries can be reduced to one.
+	 * 
+	 * @param type $subquery
+	 * @param type $query
+	 * @return boolean true if its duplicate entry 
+	 */
+	static protected function is_duplicate_query($subquery, $query)
+	{
+		foreach ($GLOBALS[self::GLOBAL_KEY_FROM_CLAUSES] as $aquery)
+		{
+			if (strstr($aquery, $subquery) != FALSE)
+			{
+				return true;
+			}
+		}
+		if (in_array($query, $GLOBALS[self::GLOBAL_KEY_FROM_CLAUSES]))
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+
+	/**
+	 * Method that deals with situations where I want to add a conditional to pull the count
+	 * @NOTE This function is used when the count() generation is more complicated.
+	 * 
+	 * Example:
+	 * 
+	 * $x->Job->Upload->count(array('file_type' => 'data_file'))
+	 * 
+	 * would create (SELECT count(*) from uploads where file_type = 'data_file' AND uploads.job_id = jobs.id) in the SELECT clause
+	 * 
+	 * @global $row
+	 * @param array $ar
+	 */
+	public function count($ar = array(null))
+	{
+		/**
+		 * Create where clause using $ar
+		 */
+		$where_clause = "1 ";
+		$alias_clause = "{$this->alias}_count";
+
+		if (!empty($ar))
+		{
+			foreach ($ar as $fieldname => $value)
+			{
+				$where_clause .= " AND {$fieldname} = '{$value}'";
+				$alias_clause = $this->construct_alias($this, self::MODE_COUNT, $value);
+			}
+		}
+
+		if (isset($this->parent->foreign_keys))
+		{
+			$parent_fkeys = $this->parent->foreign_keys;
+		}
+		else
+		{
+			return null;
+		}
+
+
+		if (isset($parent_fkeys[$this->alias]))
+		{
+			$parent_key = $parent_fkeys[$this->alias]['key'];
+			if (isset($this->parent->alias))
+				$parent_alias = $this->parent->alias;
+			else
+				$parent_alias = $this->parent->tbl;
+
+			$query = "(SELECT count(*) 
+				FROM {$this->alias} 
+				WHERE {$where_clause} 
+				AND {$parent_alias}.{$parent_key} = {$this->alias}.{$parent_fkeys[$this->alias]['fkey']}) 
+				AS {$alias_clause}";
+
+			if (!in_array($query, $GLOBALS[self::GLOBAL_KEY_SELECT_CLAUSES]))
+			{
+				$GLOBALS[self::GLOBAL_KEY_SELECT_CLAUSES][] = $query;
+			}
+		}
+	}
+
+	public function group_concat($name = null)
+	{
+		$alias_clause = $this->construct_alias($this, self::MODE_GROUPCONCAT);
+
+		if (isset($this->parent->foreign_keys))
+		{
+			$parent_fkeys = $this->parent->foreign_keys;
+		}
+		else
+		{
+			return null;
+		}
+
+		if (isset($parent_fkeys[$this->alias]))
+		{
+			$parent_key = $parent_fkeys[$this->alias]['key'];
+			if (isset($this->parent->alias))
+				$parent_alias = $this->parent->alias;
+			else
+				$parent_alias = $this->parent->tbl;
+
+			$query = "group_concat({$this->alias}.{$name} SEPARATOR ', ') as {$alias_clause}";
+
+			if (!in_array($query, $GLOBALS[self::GLOBAL_KEY_SELECT_CLAUSES]))
+			{
+				$GLOBALS[self::GLOBAL_KEY_SELECT_CLAUSES][] = $query;
+			}
+		}
+	}
+
+	/**
+	 * Creates a count alias used in MYSQL SELECT statement.
+	 * This uses parent alias, this table's alias, and optional value(if getting multiple counts from the same table)
+	 * 
+	 * @notes Modularizing this so it can potentially be customized.
+	 * 
+	 * @param type $fieldname
+	 * @param type $value
+	 * @param string $parent_alias may be null, if top-level object in an object chain
+	 * @return type 
+	 */
+	static public function construct_alias(ROCKETS_Model $model, $mode = self::MODE_COUNT, $value = null)
+	{
+		$mode_str = self::get_mode_string($mode);
+
+		$return_str = "";
+		if (isset($model->parent->alias))
+			$return_str = "{$model->parent->alias}_{$model->alias}_{$mode_str}";
+		else
+			$return_str = "{$model->alias}_{$mode_str}";
+		if ($value)
+			$return_str .= "_{$value}";
+		return $return_str;
+	}
+
+	/**
+	 * Mode string is used to construct the MYSQL field alias for count/sum/group_concat
+	 * related field data which doesn't have a tablename associated with the data in
+	 * fields META data - so we embed the table name in the fields name
+	 * 
+	 * @param type $mode 
+	 * @return string Returns the mode string
+	 */
+	static public function get_mode_string($mode)
+	{
+		switch ($mode) {
+			case self::MODE_COUNT:
+				return "count";
+			case self::MODE_SUMCOUNT:
+				return "count";
+			case self::MODE_GROUPCONCAT:
+				return "groupconcat";
+		}
+	}
+
+	/**
+	 * Retrieve result Meta data
+	 * This is useful for finding out the table name of fields when
+	 * you pull results from more than one table at a time.
+	 * 
+	 * @param <type> $result
+	 * @return Array
+	 */
+	static public function getResultFields($result)
+	{
+		$ar = array();
+		while ($row = mysql_fetch_field($result))
+		{
+			$ar[] = array(
+				'name' => $row->name, // field name
+				'table' => $row->table); // table name
+		}
+		return $ar;
+	}
+
+}
+
+?>
