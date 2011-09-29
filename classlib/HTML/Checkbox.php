58578203676881b36ff4dc4114305e90ea4aa6c0
Tetsuto <tetsuto@infotrackit.com>
2011-09-28 17:28:30 -0700

    - added the ability to wrap the whole thing in DL

--- classlib/HTML/Checkbox.php
+++ classlib/HTML/Checkbox.php
@@ -22,13 +22,15 @@ class ROCKETS_HTML_Checkbox extends ROCKETS_HTML_Form {
 	 * @param type $checked if 1, this auto-sets to checked
 	 * @param type $name  name of the checkbox
 	 */
-	static public function draw_obj($name, $obj)
+	static public function draw_obj($name, $obj, $options = array(null))
 	{
 		$input_type = self::TYPE_INPUT_CHECKBOX;
 		$selected = ($obj->$name == true) ? $selected = self::get_selected_string($input_type) : "";
 		
-		echo "<input type='hidden' name='{$name}' value='false' />" . PHP_EOL;
-		echo "<input type='{$input_type}' value='1' name='{$name}' {$selected} />" . PHP_EOL;
+		$html = "<input type='hidden' name='{$name}' value='false' />" . PHP_EOL;
+		$html .= "<input type='{$input_type}' value='1' name='{$name}' {$selected} />" . PHP_EOL;
+		
+		echo self::dl_wrap($html, $options);
 	}
 
 }
