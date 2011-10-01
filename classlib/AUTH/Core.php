baf0fc7c433e7ef440629bf9e1ab99fa16cdf1fd
Tetsuto <tetsuto@infotrackit.com>
2011-09-30 17:20:44 -0700

    - pulled roles/auth classes down into rockets

---
+++ classlib/AUTH/Core.php
@@ -0,0 +1,260 @@
+<?php
+
+/**
+ * Description of BasicAuth
+ *
+ * @author Halfdeck
+ */
+abstract class ROCKETS_AUTH_Core {
+	/**
+	 * Session keys .. e.g. $_SESSION['username']
+	 */
+	const USERNAME = 'username';
+	const PASSWORD = 'password';
+
+	/**
+	 * username is more commonly used to access table users
+	 */
+	const USER_ID = 'user_id';
+
+	/**
+	 * if users click "remember me", set a cookie so user can enter the site without logging in again.
+	 */
+	const REMEMBER_ME = 'remember_me';
+
+	/**
+	 * default session decay - 100 days
+	 */
+	const DECAY = 8640000;
+
+	/**
+	 * keys to store sessions and cookies.
+	 */
+	private static $keys = array
+		(
+		'username',
+		'password',
+		'user_id'
+	);
+
+	/**
+	 * tracks whether a user is logged in or not.
+	 * @var type
+	 */
+	public $logged_in = false;
+
+	/*	 * ********************************* */
+
+	protected function logout()
+	{
+		foreach (self::$keys as $key)
+		{
+			unset($_SESSION[$key]);
+			setcookie($key, "", time() - self::DECAY, "/");
+		}
+
+		session_unset();
+		session_destroy();
+
+		$this->logged_in = false;
+	}
+
+	/**
+	 * This runs on every page. 
+	 * STR_URL_QUERY_STRING contains the current page name.
+	 * 
+	 */
+	public function validate()
+	{
+		switch ($_REQUEST[STR_URL_QUERY_STRING]) {
+			/**
+			 * If we're on the Logout page, logout by clearing sessions and cookies.
+			 */
+			case FILE_LOGOUT:
+			/**
+			 * If we're on the Register page, logout (clear sessions and cookies)
+			 * Don't redirect: allow user to stay on this page so he/she can register
+			 */
+			case FILE_REGISTER:
+				$this->logout();
+				break;
+			/**
+			 * If we're on the login page, run ->login();
+			 */
+			case FILE_LOGIN:
+				$this->login();
+				break;
+			/**
+			 * If we're creating a new user, "pass through" - 
+			 */
+			case FILE_CREATE_USER:
+				return;
+			/**
+			 * On any other page....see if a user is logged in
+			 */
+			default:
+				if (!$this->is_logged_in())
+				{
+					ROCKETS_HTTP::redirect(RPATH_ROOT . "/" . FILE_LOGIN);
+				}
+				break;
+		}
+	}
+
+	/**
+	 * Create session and if user selects "remember me", save cookies
+	 * Password is md5 encrypted
+	 * 
+	 * @param type $options 
+	 */
+	protected function create_sessions($options = array(null))
+	{
+		foreach ($options as $key => $value)
+		{
+			if ($key == self::PASSWORD)
+			{
+				$_SESSION[$key] = md5($value);
+			}
+			else
+			{
+				$_SESSION[$key] = $value;
+			}
+			if (isset($_POST[self::REMEMBER_ME]))
+			{
+				/**
+				 * remember me checked, so remember info in a cookie
+				 * Add additional member to cookie array as per requirement
+				 */
+				setcookie($key, $value, time() + self::DECAY, "/");
+			}
+		}
+	}
+
+	/**
+	 * Exact implementation is application specific
+	 * 
+	 * @note must return true or false. If true, must also call create_sessions()
+	 * 
+	 * @return bool 
+	 */
+	abstract protected function is_valid($username, $password);
+
+	/**
+	 * @note This function gets called on login.php ONLY
+	 * So if creditials are bad, there's no need to redirect.
+	 *
+	 * @return <type>
+	 */
+
+	/**
+	 * Load session variable
+	 */
+	private function load_properties_by_session()
+	{
+		foreach ($_SESSION as $key => $value)
+		{
+			$this->$key = $value;
+		}
+	}
+
+	protected function login()
+	{
+		$username = ROCKETS_Request::get(self::USERNAME);
+		$password = ROCKETS_Request::get(self::PASSWORD);
+
+		if ($username == "" or $password == "" or $this->is_valid($username, $password) == FALSE)
+		{
+			$this->logout();
+		}
+		else
+		{
+			ROCKETS_HTTP::redirect(FILE_SUCCESS);
+		}
+	}
+
+	/**
+	 * Checks to see if a user is logged in or not.
+	 * 
+	 * @return boolean True if user is logged in, False otherwise
+	 */
+	public function is_logged_in()
+	{
+		/** if username and password are saved in sessions, user is logged in * */
+		if (isset($_SESSION[self::USERNAME]) AND isset($_SESSION[self::PASSWORD]))
+		{
+			$this->logged_in = true;
+			$this->load_properties_by_session();
+			return true;
+		}
+		elseif (isset($_COOKIE[self::USERNAME]) && isset($_COOKIE[self::PASSWORD]))
+		{
+			if ($this->is_valid($username, $password))
+			{
+				ROCKETS_HTTP::redirect(FILE_SUCCESS);
+			}
+			else
+			{
+				$this->logout();
+				return false;
+			}
+		}
+		else
+		{
+			return false;
+		}
+	}
+
+	/**
+	 * Get a user's ID
+	 * 
+	 * @note can't use __get here because its a static call
+	 * 
+	 * @return type 
+	 */
+	static public function get_user_id()
+	{
+		if (isset($_SESSION[self::USER_ID]))
+			return $_SESSION[self::USER_ID];
+		else
+			return null;
+	}
+
+	/**
+	 * Get a user's username
+	 * 
+	 * @return type 
+	 */
+	static public function get_username()
+	{
+		if (isset($_SESSION[self::USERNAME]))
+			return $_SESSION[self::USERNAME];
+		else
+			return null;
+	}
+
+	/**
+	 * Default __get
+	 * First tries to fetch value from SESSIONS
+	 * 
+	 * @param type $name
+	 * @return type
+	 */
+	public function __get($name)
+	{
+		switch ($name) {
+			default:
+				if (isset($this->$name))
+				{
+					return $this->$name;
+				}
+				else
+				{
+					$this->$name = $_SESSION[$name];
+					return $this->$name;
+				}
+		};
+	}
+
+}
+
+?>
---
+++ classlib/AUTH/Roles.php
@@ -0,0 +1,175 @@
+<?php
+
+/**
+ * Description of Roles
+ * 
+ * @abstraction - need to detach from application-specific model classes
+ *
+ * @author Tetsuto
+ */
+abstract class ROCKETS_AUTH_Roles extends ROCKETS_AUTH_Core {
+
+	/**
+	 * A list of roles (string names, not IDs)
+	 * Used to map role names to string names, needed to dynamically display listboxes
+	 * depending on role
+	 * 
+	 * @var type 
+	 */
+	public $access_rights = array();
+	static public $permissions = array(
+		'read' => 'read',
+		'write' => 'create',
+		'create' => 'create',
+		'update' => 'update'
+	);
+
+	/*
+	 * Permission constants
+	 */
+	const PERMISSION_READ = 'read';
+	const PERMISSION_WRITE = 'create';
+	const PERMISSION_CREATE = 'create';
+	const PERMISSION_UPDATE = 'update';
+	
+	public function __construct()
+	{
+		
+	}
+
+
+	/**
+	 * If [role name] is in an array of roles for this user, return true
+	 * else return false
+	 * 
+	 * @param string $role_string
+	 * @return boolean
+	 */
+	public function is_role_name($role_string)
+	{
+		if (in_array($role_string, $this->role_names))
+			return true;
+		else
+			return false;
+	}
+	
+	/**
+	 * Default __get
+	 * @param type $name
+	 * @return type
+	 */
+	public function __get($name)
+	{
+		switch ($name) {
+			case "roles":
+				if (isset($this->$name))
+					return $this->$name;
+				else
+				{
+					$this->load_roles();
+					return $this->roles;
+				}
+				break;
+			case "role_names":
+				if (isset($this->role_names))
+					return $this->role_names;
+				else
+				{
+					$this->load_roles();
+					return $this->role_names;
+				}
+				break;
+			default:
+				parent::__get($name);
+				break;
+		};
+	}
+	
+	/**
+	 * Get roles and role names - specific implementation is app specific
+	 */
+	abstract public function load_roles();
+
+	/**
+	 * Checks if the user has Read access to the resource, identified by $task_name
+	 * 
+	 * @param type $task_name
+	 * @param string $type
+	 * @param string $field_name
+	 * @return type 
+	 */
+	public function check_permission($name, $type = JOB_MODEL_Task::TYPE_PAGE, $field_name = null, $permission_type)
+	{
+		/**
+		 * access_rights contain an array of available task descriptions. If item
+		 * isn't in this array, don't give the user access.
+		 * This should be a whitelist - otherwise logoff exposes hidden UI
+		 */
+		if (empty($this->access_rights[$name][$type][$field_name]))
+		{
+			return false;
+		}
+		
+		/**
+		 * Default permission type is READ
+		 */
+		if($permission_type == NULL) $permission_type = self::$permissions['read'];
+		
+		//echo $name ." " .$field_name ." " .$this->access_rights[$name][$type][$field_name] ." {$permission_type}<br>";
+		/**
+		 * Is readable
+		 */
+		if (strstr($this->access_rights[$name][$type][$field_name], $permission_type))
+		{
+			return true;
+		}
+		else
+		/**
+		 * No match 
+		 */
+		{
+			return false;
+		}
+	}
+
+	/**
+	 * Given a rights matrix array and a mysql row, add the row data to the matrix.
+	 * If there's already an entry with the same task ID, merge the rights
+	 * 
+	 * @param string $rightsMatrix structure: array("name" => array('type' => array(field_name => ...) = rights
+	 * @param string $row
+	 */
+	static function add_rights(Array $rightsMatrix, Array $row)
+	{
+		/**
+		 * If task already exists in the Rights Matrix, merge.
+		 */
+		if (isset($rightsMatrix[$row['name']][$row['type']][$row['field_name']]))
+		{
+			$rightsMatrix[$row['name']][$row['type']][$row['field_name']] = self::merge_rights_strings($rightsMatrix[$row['name']][$row['type']][$row['field_name']], $row['rights']);
+		}
+		/**
+		 * New task, so just add to the Rights Matrix
+		 */
+		else
+		{
+			$rightsMatrix[$row['name']][$row['type']][$row['field_name']] = $row['rights'];
+		}
+		return $rightsMatrix;
+	}
+
+	/**
+	 * Merge two strings and return a combined string
+	 * Example: $string1 = "create,delete,edit"; $string2 = "create"; return value => "create,delete,edit"
+	 * 
+	 * @param type $string1
+	 * @param type $string2
+	 * @return type 
+	 */
+	static function merge_rights_strings($string1, $string2)
+	{
+		return ROCKETS_String::merge_comma_delimited_strings($string1, $string2);
+	}
+}
+
+?>
