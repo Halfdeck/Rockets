b29d6274cf12fd0e3976fff9e82cafe3a9fdde6b
Tetsuto <tetsuto@infotrackit.com>
2011-10-04 14:10:47 -0700

    - created an HTML Button class

---
+++ classlib/HTML/Button.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * Description of Textbox
+ *
+ * @author Aaron
+ */
+class ROCKETS_HTML_Button extends ROCKETS_HTML_Form 
+{	
+    /**
+     * Auto-draws a button
+     * 
+	 * Required: $options['name'],$options['value']
+	 * 
+     * @param type $name name of the INPUT
+     * @param type $obj object used to retrieve value
+	 * @param boolean $options['read only'] activate read only 
+	 * @param int $options['size'] custom input size
+     */
+    static public function draw($options = array(null))
+    {
+		$id = (isset($options['id']) && $options['id'] == TRUE) ? "id='{$options['id']}'" : "";
+		$class = (isset($options['class']) && $options['class'] == TRUE) ? "class='{$options['class']}'" : "";
+		$type = (isset($options['type'])) ? "type='{$options['type']}'" : "type='button'";
+		$name = "name='{$options['name']}'";
+		$element = 'BUTTON';
+		$value = $options['value'];
+		
+        $html = "<{$element} {$id} {$class} {$type} {$name}>{$value}</{$element}>";
+		
+		echo self::dl_wrap($html, $options);
+    }
+}
+
+?>
