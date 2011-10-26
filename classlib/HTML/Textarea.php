a55f466e3d4500ed2d6d4e90eac5a8eecc8865b3
Tetsuto <tetsuto@infotrackit.com>
2011-10-26 13:32:19 -0700

    - added an options array so I can add an ID and CLASS

--- classlib/HTML/Textarea.php
+++ classlib/HTML/Textarea.php
@@ -13,9 +13,12 @@ class ROCKETS_HTML_Textarea extends ROCKETS_HTML_Form {
 	 * @param type $name name of the INPUT
 	 * @param type $obj object used to retrieve value
 	 */
-	static public function draw($name, $obj, $col, $row)
+	static public function draw($name, $obj, $col, $row, $options = array(null))
 	{
-		echo "<textarea name='{$name}' cols='{$col}' rows='{$row}'>{$obj->$name}</textarea>";
+		$id = (isset($options['id'])) ? "id='{$options['id']}'" : null;
+		$class = (isset($options['class'])) ? "class='{$options['class']}'" : null;
+		
+		echo "<textarea name='{$name}' cols='{$col}' rows='{$row}' {$id} {$class}>{$obj->$name}</textarea>";
 	}
 
 }
