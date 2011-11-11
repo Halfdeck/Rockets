d89d1320c8377a16ea11957a94b7e770a3f38711
Tetsuto <tetsuto@infotrackit.com>
2011-11-11 14:04:51 -0800

    - added draw_boolean method, which takes true/false value instead of an object.

--- classlib/HTML/Checkbox.php
+++ classlib/HTML/Checkbox.php
@@ -35,6 +35,30 @@ class ROCKETS_HTML_Checkbox extends ROCKETS_HTML_Form {
 		
 		return self::dl_wrap($html, $options);
 	}
+	
+	/**
+	 * @mod uses boolean value instead of object
+	 * 
+	 * Auto-draws a checkbox input, using an object
+	 * 
+	 * Usage: JOB_HTML_Form::draw_checkbox_obj('input_name', $user) where $user is a class JOB_MODEL_User
+	 * 
+	 * @param type $checked if 1, this auto-sets to checked
+	 * @param type $name  name of the checkbox
+	 */
+	static public function draw_boolean($name, $boolean, $options = array(null))
+	{
+		$input_type = self::TYPE_INPUT_CHECKBOX;
+		
+		$value = (isset($options['value'])) ? $options['value'] : 1;
+		$null_value = (isset($options['null value'])) ? $options['null value'] : 'false';
+		$selected = ($boolean) ? $selected = self::get_selected_string($input_type) : "";
+		
+		$html = "<input type='hidden' name='{$name}' value='{$null_value}' />" . PHP_EOL;
+		$html .= "<input type='{$input_type}' value='{$value}' name='{$name}' {$selected} />" . PHP_EOL;
+		
+		return self::dl_wrap($html, $options);
+	}
 
 }
 
