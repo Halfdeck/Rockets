24263b201949cb3f4f03ae640f0291fb56d4e063
Tetsuto Yabuki <halfdeck@gmail.com>
2013-04-12 14:18:17 -0700

    Added a new draw_value() method in Textarea that takes a value instead of an object.

--- classlib/HTML/Textarea.php
+++ classlib/HTML/Textarea.php
@@ -22,6 +22,16 @@ class ROCKETS_HTML_Textarea extends ROCKETS_HTML_Form {
 		
 		return "<textarea name='{$name}' cols='{$col}' rows='{$row}' {$id} {$class} {$readonly}>{$html}</textarea>";
 	}
+	
+	static public function draw_value($name, $value, $col, $row, $options = array(null))
+	{
+		$id = (isset($options['id'])) ? "id='{$options['id']}'" : null;
+		$class = (isset($options['class'])) ? "class='{$options['class']}'" : null;
+		$readonly = (isset($options['read only']) && $options['read only'] == TRUE) ? self::STR_READ_ONLY : "";
+		$html = (isset($value)) ? $value : null;
+		
+		return "<textarea name='{$name}' cols='{$col}' rows='{$row}' {$id} {$class} {$readonly}>{$html}</textarea>";
+	}
 
 }
 
